57e15f69ad0a86318d9639f3de607e6c1ce603e6
Bryson <bryson.meiling@gmail.com>
2021-07-06 13:42:33 -0400

    project v1 finished. Shorter than I thought but works suprislingly well for 3 lines of code that actually do the work. Will use to see the contents of these pages that have paywall blockers!

---
+++ news_paywall_avoider.py
@@ -0,0 +1,22 @@
+from os import error
+import urllib.request as req
+import webbrowser as wb
+from bs4 import BeautifulSoup
+from sys import argv
+
+def main(news_url):
+    
+    USAGE_MESSAGE = "="*20 + "\nUSAGE:\npython news_paywall_avoider.py URL\n"+20*"="
+    # fake_ai_news = "https://www.technologyreview.com/2020/08/14/1006780/ai-gpt-3-fake-blog-reached-top-of-hacker-news/"
+    # nytimes = "https://www.nytimes.com/2018/11/14/climate/california-wildfires-and-reinventing-air-conditioners-climate-newsletter.html"
+    try:
+        p_elements = BeautifulSoup(req.urlopen(news_url).read(), 'html.parser').find_all("p")
+        for ele in p_elements:
+            print(ele.text, end=" ")
+    except ValueError as e:
+        print(f"\nBAD URL: Might need https:// in front\n{USAGE_MESSAGE}\n{e}")
+    except:
+        print(f"Unknown Error\n{USAGE_MESSAGE}\n")
+
+if "__main__" == __name__:
+    main(argv[1])
